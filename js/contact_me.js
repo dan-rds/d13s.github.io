db15553eab3615a295ff18b8e38c1d6f3360a8eb
daniel <ddrichar@ucc.edu>
2017-08-12 09:37:13 -0700

    working on mail server

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "https://formspree.io/dan@richardsgroup.us",
                 type: "POST",
                 data: {
                     name: name,
